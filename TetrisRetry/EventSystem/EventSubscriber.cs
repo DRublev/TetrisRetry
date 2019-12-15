8b4118e91eafe3644a52c61a19a53036301ab67b
Daniil <aringai09@gmail.com>
2019-12-15 20:42:08 +0300

    Soft event subscribing system
    With this u can subscribe on events with calling one method instead of creating delegates

---
+++ TetrisRetry/EventSystem/EventSubscriber.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+
+namespace TetrisRetry.EventSystem
+{
+	public class EventSubscriber
+	{
+		private BindingFlags flags =
+			BindingFlags.Public |
+			BindingFlags.Static |
+			BindingFlags.NonPublic |
+			BindingFlags.Instance |
+			BindingFlags.FlattenHierarchy;
+
+		public EventSubscriber()
+		{
+			MethodInfo[] methods = this.GetType().GetMethods(flags);
+
+			foreach(MethodInfo info in methods)
+			{
+				if(info.GetCustomAttributes(typeof(CustomEventHandler.MessageHandler), true).Length != 0)
+				{
+					CustomEventHandler.Instance.RegisterHandler(this, info);
+				}
+			}
+		}
+
+		protected void Subscribe(string methodName)
+		{
+			MethodInfo info = this.GetType().GetMethod(methodName, flags);
+
+			CustomEventHandler.Instance.RegisterHandler(this, info);
+		}
+
+		protected void Unsubscribe(string methodName)
+		{
+			MethodInfo info = this.GetType().GetMethod(methodName, flags);
+
+			CustomEventHandler.Instance.UnregisterHandler(this, info);
+		}
+	}
+}
