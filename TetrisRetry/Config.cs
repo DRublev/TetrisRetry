f94cb948513d9f35bd8af8efebc281e8aae46c53
Daniil <aringai09@gmail.com>
2019-12-16 17:01:37 +0300

    Figure creating #100DaysOfCode
    Add figure random creating. U need just create new obj typeof Figure.

--- TetrisRetry/Config.cs
+++ TetrisRetry/Config.cs
@@ -4,5 +4,6 @@
 	{
 		public const int FIELD_HEIGHT = 16;
 		public const int FIELD_WIDTH = 12;
+		public const char FILLING = "*";
 	}
 }
--- TetrisRetry/Figure.cs
+++ TetrisRetry/Figure.cs
@@ -1,10 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace TetrisRetry
 {
-	class Figure
+	enum FigureTypes
 	{
+		I, T, J, L, S, Z, O
+	}
+	public class Figure
+	{
+		public List<String> figure { get; private set; } = new List<string>();
+		public Figure()
+		{
+			Random random = new Random();
+			figure = Create((FigureTypes) random.Next(7));
+		}
+		private List<string> Create(FigureTypes type)
+		{
+			List<string> figure = new List<string>();
+
+			switch(type)
+			{
+				case FigureTypes.I:
+					figure.Add(new String(Config.FILLING, 1));
+					figure.Add(new String(Config.FILLING, 1));
+					figure.Add(new String(Config.FILLING, 1));
+					figure.Add(new String(Config.FILLING, 1));
+					break;
+				case FigureTypes.J:
+					figure.Add($" {Config.FILLING}");
+					figure.Add($" {Config.FILLING}");
+					figure.Add(new String(Config.FILLING, 2));
+					break;
+				case FigureTypes.L:
+					figure.Add($"{Config.FILLING} ");
+					figure.Add($"{Config.FILLING} ");
+					figure.Add(new String(Config.FILLING, 2));
+					break;
+				case FigureTypes.O:
+					figure.Add(new string(Config.FILLING, 2));
+					figure.Add(new string(Config.FILLING, 2));
+					break;
+				case FigureTypes.S:
+					figure.Add($" {Config.FILLING}{Config.FILLING}");
+					figure.Add($"{Config.FILLING}{Config.FILLING} ");
+					break;
+				case FigureTypes.T:
+					figure.Add($" {Config.FILLING} ");
+					figure.Add($"{Config.FILLING}{Config.FILLING}{Config.FILLING}");
+					break;
+				case FigureTypes.Z:
+					figure.Add($"{Config.FILLING}{Config.FILLING} ");
+					figure.Add($" {Config.FILLING}{Config.FILLING}");
+					break;
+			}
+
+			return figure;
+		}
 	}
 }
